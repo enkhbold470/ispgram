4c452e58281a6d42e1b0a302da2ad3034cb0ab86
enkhbold470 <enkhbold470@gmail.com>
2025-10-25 08:25:03 -0700

    Update next.config.ts to enable image optimization and add new remote image patterns. Enhance package.json with Prisma seed command for database seeding.

--- next.config.ts
+++ next.config.ts
@@ -4,11 +4,17 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'va6iw0qi88gfmfrc.public.blob.vercel-storage.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'placekeanu.com',
+      },
+      
     ],
   },
 };
